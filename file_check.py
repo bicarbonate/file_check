d592ebaecce232b4e04bf235b28a729227b33d7c
bicarbonate <invalid.path@gmail.com>
2017-06-29 10:13:41 -0600

    New file

---
+++ file_check.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from pathlib import Path
+import os.path
+#os.path.isfile()
+
+#adr01_file = Path(/chroot/monitor/adr01/upload/)
+#chpt_file = Path(/chroot/monitor/chpt/upload/monitor*.pgp)
+#emb01_file = Path(/chroot/monitor/emb01/upload/)
+#exp01_file = Path(/chroot/monitor/exp01/upload/)
+#iix01_file = Path(/chroot/monitor/iix01/upload/)
+
+adr01_file = Path(/home/bhart/Documents/Python/home/adr01/upload/monitor*.pgp)
+if adr01_file.is_file():
+    
+chpt_file = Path(/home/bhart/Documents/Python/home/chpt/upload/monitor*.pgp)
+if adr01_file.is_file():
+    
+emb01_file = Path(/home/bhart/Documents/Python/home/emb01/upload/monitor*.pgp)
+if adr01_file.is_file():
+    
+exp01_file = Path(/home/bhart/Documents/Python/home/exp01/upload/monitor*.pgp)
+if adr01_file.is_file():
+    
+iix01_file = Path(/home/bhart/Documents/Python/home/iix01/upload/monitor*.pgp)
+if adr01_file.is_file():
+    
+
