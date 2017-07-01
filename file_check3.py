e66a5839c2e1046b0c7cf2abcf499cf913924fc3
bicarbonate <invalid.path@gmail.com>
2017-07-01 00:21:49 -0600

    fixing stupid fucking git bullshit

--- file_check3.py
+++ file_check3.py
@@ -1,10 +1,56 @@
 # -*- coding: UTF-8 -*-
-
+#
+# Jul 2017
+#
 import glob
 import os
+from datetime import datetime
+
+
+monitor_files = []
 
 os.chdir("/home/skeer/Documents/Projects/Python_temp/home/adr01/upload/")
 for file in glob.glob("*.pgp"):
-    print(file)
-    # noinspection PyUnresolvedReferences
-    adr01.file = (file()
+    if os.path.isfile(file):
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(file))
+    else:
+        last_modified_date = datetime.fromtimestamp(0)
+    monitor_files.append("adr01 | " + file + (" | ") + str(last_modified_date) + "\n")
+
+os.chdir("/home/skeer/Documents/Projects/Python_temp/home/chpt/upload/")
+for file in glob.glob("*.pgp"):
+    if os.path.isfile(file):
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(file))
+    else:
+        last_modified_date = datetime.fromtimestamp(0)
+    monitor_files.append("chpt | " + file + (" | ") + str(last_modified_date) + "\n")
+
+os.chdir("/home/skeer/Documents/Projects/Python_temp/home/emb01/upload/")
+for file in glob.glob("*.pgp"):
+    if os.path.isfile(file):
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(file))
+    else:
+        last_modified_date = datetime.fromtimestamp(0)
+    monitor_files.append("emb01 | " + file + (" | ") + str(last_modified_date) + "\n")
+
+os.chdir("/home/skeer/Documents/Projects/Python_temp/home/exp01/upload/")
+for file in glob.glob("*.pgp"):
+    if os.path.isfile(file):
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(file))
+    else:
+        last_modified_date = datetime.fromtimestamp(0)
+    monitor_files.append("exp01 | " + file + (" | ") + str(last_modified_date) + "\n")
+
+os.chdir("/home/skeer/Documents/Projects/Python_temp/home/iix01/upload/")
+for file in glob.glob("*.pgp"):
+    if os.path.isfile(file):
+        last_modified_date = datetime.fromtimestamp(os.path.getmtime(file))
+    else:
+        last_modified_date = datetime.fromtimestamp(0)
+    monitor_files.append("iix01 | " + file + (" | ") + str(last_modified_date) + "\n")
+
+print("\n".join(monitor_files))
+
+
+ #   fromaddr = ("DZ@mt.gov")
+  #  toaddrs = ("invalid.path@gmail.com")
